56489a769f389112870b3afd26e6e1c2b1290e8b
JFC444 <jojo.cotta99@gmail.com>
2025-05-23 11:51:36 +0200

    3.1.6

---
+++ max.py
@@ -0,0 +1,31 @@
+name = "Max Mustermann"
+alter = 30
+geschlecht = "männlich"
+groesse = "1,80m"
+gewicht = "75kg"
+augenfarbe = "braun"
+haarfarbe = "schwarz"
+beruf = "Softwareentwickler"
+hobby = "Lesen"
+charakter = "freundlich"
+
+# Satz-Ausgabe
+print(f"{name} ist ein {alter} Jahre alter {geschlecht}er {beruf}.")
+print(f"Er ist {groesse} groß, wiegt {gewicht}, hat {augenfarbe}e Augen und {haarfarbe}e Haare.")
+print(f"In seiner Freizeit liest er gerne und gilt als sehr {charakter}.")
+
+name = "Guts"
+alter = 28
+geschlecht = "männlich"
+groesse = "2.06m"
+gewicht = "150kg"
+augenfarbe = "grün"
+haarfarbe = "schwarz"
+beruf = "Berserker"
+hobby = "Demonen jagen"
+charakter = "fucked up"
+
+# Satz-Ausgabe
+print(f"{name} ist ein {alter}-jährige {geschlecht}e {beruf}.")
+print(f"Er ist {groesse} groß, wiegt {gewicht}, hat {augenfarbe}e Augen und {haarfarbe}e Haare.")
+print(f"Er killt leidenschaftlich gerne und ist bekannt für seine {charakter} Art.")
---
+++ youtube.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("7")
+print(7 + 4)
+print(3 + 2 * 2 -1)
+print(2.4 * 5)
+name = "Guts"
+print(name)
