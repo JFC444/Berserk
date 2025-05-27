b7d6e61a2555fb2114f2dd2de7636374828116e0
JFC444 <jojo.cotta99@gmail.com>
2025-05-27 13:41:37 +0200

    3.2.8mittel

---
+++ 3.2.8next.py
@@ -0,0 +1,5 @@
+zahlen = [4, 7, 2, 9, 1, 5, 3]
+durchschnitt = sum(zahlen) / len(zahlen)
+print("Durchschnitt:", durchschnitt)
+umgekehrt = list(reversed(zahlen))
+print("Liste in umgekehrter Reihenfolge:", umgekehrt)
