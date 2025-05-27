6857b99ef8a96d8b8406bbd88717a2cce7dfa501
JFC444 <jojo.cotta99@gmail.com>
2025-05-27 14:13:26 +0200

    2.möglichkeit

--- 3.2.8next.py
+++ 3.2.8next.py
@@ -3,3 +3,11 @@ durchschnitt = sum(zahlen) / len(zahlen)
 print("Durchschnitt:", durchschnitt)
 umgekehrt = list(reversed(zahlen))
 print("Liste in umgekehrter Reihenfolge:", umgekehrt)
+
+
+zahlen = [4, 7, 2, 9, 1, 5, 3]
+durchschnitt = sum(zahlen) / len(zahlen)
+print("Durchschnitt:", durchschnitt)
+
+umgekehrt = zahlen[::-1]  # Hier wird die Liste rückwärts gelesen
+print("Liste in umgekehrter Reihenfolge:", umgekehrt) 
---
+++ test.py
@@ -0,0 +1,6 @@
+zahlen = [4, 7, 2, 9, 1, 5, 3]
+durchschnitt = sum(zahlen) / len(zahlen)
+print("Durchschnitt:", durchschnitt)
+
+umgekehrt = zahlen[::-1]  # Hier wird die Liste rückwärts gelesen
+print("Liste in umgekehrter Reihenfolge:", umgekehrt)
